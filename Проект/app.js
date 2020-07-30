810e82c46f134b7d10874af2d1c2920081f27913
Artem Knyaginin <artemknaginin3@gmail.com>
2020-07-30 22:03:27 +0300

    Update project files

--- Проект/app.js
+++ Проект/app.js
@@ -1,5 +1,32 @@
 $(function() {
 	
+	let lightBox = $("#lightBox");
+	let scrollPosition = $(window).scrollTop();
+	let darkBg = $("#darkBg");
+	
+	$(".btn").on("click", function(event) {
+		event.preventDefault();
+		
+		darkBg.toggleClass("show");
+		lightBox.toggleClass("show");
+	});
+	
+	$("#darkBg").on("click", function(event) {
+		event.preventDefault();
+		
+		darkBg.toggleClass("show");
+		lightBox.toggleClass("show");
+	});
+	
+	$(window).on("scroll load resize", function(){
+		scrollPosition = $(this).scrollTop();
+		
+		lightBox.addClass("fixed");
+		darkBg.addClass("fixed");
+	});
+	
+	
+	
 	let nav = $("#nav");
 	$("#navToggle").on("click", function(event) {
 		event.preventDefault();
